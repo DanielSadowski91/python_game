4306270316b789b436449f654a329b21eedd9be1
Daniel <d.sadowski91@gmail.com>
2021-01-05 21:16:33 +0100

    window pygame

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
-print("Hello")
-print("Why can't it upload gfddsadsadas")
+import sys
+
+import pygame
+
+class AlienInvasion:
+    """Class for resources managment and a way for working game"""
+
+    def __init__(self):
+        """Initializing the game and opening its resources"""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """
+        Starting the main loop
+        """
+        while True:
+            #Waiting for any key
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            
+            #Displaying the last modified screen
+            pygame.display.flip()
+    
+if __name__ == '__main__':
+    #activate the game
+    ai = AlienInvasion()
+    ai.run_game()
+
