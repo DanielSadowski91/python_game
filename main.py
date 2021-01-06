ff2feaf2423359f318c8d18dcf5b4e0c6349fa23
Daniel <d.sadowski91@gmail.com>
2021-01-06 11:38:39 +0100

    settings in main

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+"""Game invasion"""
 import sys
 
 import pygame
+
+from settings import Settings
 
 class AlienInvasion:
     """Class for resources managment and a way for working game"""
@@ -8,8 +11,10 @@ class AlienInvasion:
     def __init__(self):
         """Initializing the game and opening its resources"""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
@@ -21,12 +26,14 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            
+
+            #screen refreshing during each loop
+            self.screen.fill(self.settings.bg_color)
+
             #Displaying the last modified screen
             pygame.display.flip()
-    
+
 if __name__ == '__main__':
     #activate the game
     ai = AlienInvasion()
     ai.run_game()
-
