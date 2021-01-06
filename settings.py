e1e81b1d8c53ac4a0e83ea31801767c91d385d7b
Daniel <d.sadowski91@gmail.com>
2021-01-06 11:38:23 +0100

    settings

---
+++ settings.py
@@ -0,0 +1,12 @@
+""" Settings for alien invasion"""
+class Settings:
+    """
+    Class for game settings
+    """
+
+    def __init__(self):
+        """Starting game settings"""
+        #Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
