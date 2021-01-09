1fe9b1d167d63ce12396f39903318848e2e0f0dd
Daniel <d.sadowski91@gmail.com>
2021-01-09 19:07:35 +0100

    bullet

--- settings.py
+++ settings.py
@@ -13,3 +13,9 @@ class Settings:
 
         #settings for ship
         self.ship_speed = 1.5
+
+        #Settings for bullets
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
